6c4f4c3a29744c50736037a33eb6744bef495b83
BrunoHenSL <brunosouz16@hotmail.com>
2022-05-09 22:17:50 -0300

    Update calc.js

--- calc.js
+++ calc.js
@@ -54,32 +54,66 @@ function removeLastNumber(){
 
 ////////////////////////////////
 
-function calcular(){
-  var formulario = document.getElementById("formulario");
+function validaEntrada(args){
+  for (let i = 0; i < arguments.lenght; i++){if(window.CP.shouldStopExecution(0)) break;
+    if (!!arguments[i] == false || arguments[i] < 0){
+      return false;
+    }
+  }window.CP.exitedLoop(0);
+  return true;
+}
 
-  var kilos = +formulario.kilos.value;
-  var metros = +formulario.metros.value;
-  var centimetros = +formulario.centimetros.value;
+function calcularIMC (kilos, altura){
+  altura = altura / 100;
+  return (
+    kilos / (altura * altura));
+}
 
-  var altura = (metros * 100 + centimetros) / 100;
+const formCalcularIMC = document.getElementById('form');
 
-  var imc = kilos / (altura * altura);
+formCalcularIMC.addEventListener('submit', function(event){
+  event.preventDefault();
+  const kilos = parseFloat(document.getElementById('kilos').value);
+  const altura = parseFloat(document.getElementById('altura').value);
 
-  formulario.imc.value = imc.toFixed(2);
-
-  if(imc < 29){
-    alert ('Você está abaixo do peso!');
-  }else if (imc > 20 && imc <= 25){
-    alert ("Pesoa ideal");
-  }else if (imc >25 && imc <= 30){
-    alert ("Sobrepeso");
-  }else if (imc >30 && imc <= 35){
-    alert ("Obesidade Moderada");
-  }else if (imc >35 && imc <= 40){
-    alert ("Obesidade Severa.");
-  }else if (imc >40 && imc <= 50){
-    alert ("Obesidade Morbida");
+  if (validaEntrada(kilos, altura)){
+    const imc = calcularIMC(kilos, altura);
+    document.getElementById('imc').value = parseFloat(imc).toFixed(2);
+    verificarIMC(imc);
   }else{
-    alert ('Gordo');
+    document.getElementById('imc').value = "## ERROR ##";
+  }
+});
+
+function verificarIMC(imc){
+  if (imc < 17){
+    createMessage ("Muito abaixo do peso", "alert")
+  }else if (imc >= 17 && imc <= 18.49){  
+    createMessage("Abaixo do peso", "warning")
+  }else if (imc >= 18.5 && imc <= 24.99){
+    createMessage("Peso normal", "sucess")
+  }else if (imc >= 25 && imc <= 29.99){
+    createMessage("Acima do peso", "warning")
+  }else if (imc >= 30 && imc <= 34.99){
+    createMessage("Obesidade I", "alert")
+  }else{
+    createMessage("Obesidade II", "danger")
+  }
+}
+
+function createMessage(msg, type){
+  document
+    .querySelector("body")
+    .insertAdjacentHTML("beforebegin", `<div class='message ${type}'>${msg}</div>`);
+  
+  setTimeout(function(){
+    deleteMessage();
+  }, 3000);
+}
+
+function deleteMessage(){
+  const list = document.querySelectorAll(".message");
+  for (const item of list){
+    item.remove();
   }
 }
